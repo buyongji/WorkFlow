67ed5953610fe2364ad8592c9a25c84ac983882e
buyongji <buiiii@163.com>
2014-10-28 23:18:50 +0800

    Initial commit

---
+++ CodeTest/BU.BOS/Context.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BU.BOS
+{
+    public sealed class Context
+    {
+
+        public readonly CultureInfo DefaultLocale;
+        public readonly string CharacterSet;
+        public string ComputerName
+        {
+            get;
+            set;
+        }
+
+
+        public object Clone();
+        public Context GetQueryDBContext();
+        public void OnDeserialization();
+      
+    }
+}
